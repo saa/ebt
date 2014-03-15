ee35110b58d41da97afaf9e6153811294d1470be
Sergey Abramyan <sa.abramyan@gmail.com>
2014-03-15 23:36:09 +0400

    Add year

--- src/ebt_date.erl
+++ src/ebt_date.erl
@@ -17,5 +17,6 @@ date_str_ru() ->
         11 => " ноября ",
         12 => " декабря "
        },
+    YearStr = " года",
     {{Y, M, D}, _} = erlang:localtime(),
-    unicode:characters_to_binary(integer_to_list(D) ++ maps:get(M, Months) ++ integer_to_list(Y)).
+    unicode:characters_to_binary(integer_to_list(D) ++ maps:get(M, Months) ++ integer_to_list(Y) ++ YearStr).
