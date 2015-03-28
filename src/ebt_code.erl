0dd19296e78d16db7d72b7c87dda367e03c6b753
Sergey Abramyan <sa.abramyan@gmail.com>
2015-03-28 21:24:45 +0300

    Add ebt_code

---
+++ src/ebt_code.erl
@@ -0,0 +1,13 @@
+-module(ebt_code).
+
+-export([priv_dir/1]).
+
+-spec priv_dir(atom()) -> string().
+priv_dir(App) ->
+    case code:priv_dir(App) of
+        {error, bad_name} ->
+            Ebin = filename:dirname(code:which(App)),
+            filename:join(filename:dirname(Ebin), "priv");
+        PrivDir ->
+            PrivDir
+    end.
